1ec0391e347850b4e9a84e1778adeda54c09f2fa
Baurzhan <iowathe3rd@proton.me>
2024-05-21 01:24:45 +0500

    initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {MatSlideToggle} from "@angular/material/slide-toggle";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatSlideToggle],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
