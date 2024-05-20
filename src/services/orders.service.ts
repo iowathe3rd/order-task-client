57e707c736abb2ca16b13c0e18751f12ef89fcd3
Baurzhan <iowathe3rd@proton.me>
2024-05-21 01:34:12 +0500

    feat(services, types): added types for Order entity, added service with logic

---
+++ src/services/orders.service.ts
@@ -0,0 +1,52 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import {Observable, throwError} from 'rxjs';
+import { catchError } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class OrderService {
+  private apiUrl = 'https://localhost:5154/orders'; // Замените на URL вашего API
+
+  constructor(private http: HttpClient) { }
+
+  // Получить список всех заказов
+  getAllOrders(): Observable<Order[]> {
+    return this.http.get<Order[]>(this.apiUrl)
+      .pipe(
+        catchError(this.handleError)
+      );
+  }
+
+  createOrder(order: Omit<Order, "id">): Observable<Order> {
+    return this.http.post<Order>(this.apiUrl, order)
+      .pipe(
+        catchError(this.handleError)
+      );
+  }
+
+  // Получить заказ по его ID
+  getOrderById(id: string): Observable<Order> {
+    const url = `${this.apiUrl}/${id}`;
+    return this.http.get<Order>(url)
+      .pipe(
+        catchError(this.handleError)
+      );
+  }
+
+  // Удалить заказ по его ID
+  deleteOrder(id: string): Observable<any> {
+    const url = `${this.apiUrl}/${id}`;
+    return this.http.delete<any>(url)
+      .pipe(
+        catchError(this.handleError)
+      );
+  }
+
+  // Обработка ошибок HTTP-запросов
+  private handleError(error: any): Observable<never> {
+    console.error('An error occurred:', error);
+    throw new Error(error);
+  }
+}
---
+++ src/types/index.d.ts
@@ -0,0 +1,6 @@
+declare interface Order {
+  id: string;
+  orderName: string;
+  quantity: number;
+  unitPrice: number;
+}
